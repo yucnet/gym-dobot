d7ed5a1a65761c745ddd283c2a6e507b19b41f80
WarrG3X <warrier.abhishek@gmail.com>
2018-06-14 16:54:39 +0530

    Added fetch controller

---
+++ fetch_controller.py
@@ -0,0 +1,33 @@
+import numpy as np
+from tkinter import *
+
+import gym
+
+
+
+
+env = gym.make("FetchPickAndPlace-v1")
+env.reset()
+env.render()
+
+def update_env(event):
+    action = np.array([w1.get(),w2.get(),w3.get(),w4.get()])
+    observation, reward, done, info = env.step(action)
+    print(action)
+    
+
+root = Tk()
+root.title("Fetch Controller")
+root.geometry("300x300")
+w1 = Scale(root, from_=-1, to=1, orient=HORIZONTAL,label="X Coordinate",resolution=0.01,command=update_env)
+w1.pack()
+w2 = Scale(root, from_=-1, to=1, orient=HORIZONTAL,label="Y Coordinate",resolution=0.01,command=update_env)
+w2.pack()
+w3 = Scale(root, from_=-1, to=1, orient=HORIZONTAL,label="Z Coordinate",resolution=0.01,command=update_env)
+w3.pack()
+w4 = Scale(root, from_=-1, to=1, orient=HORIZONTAL,label="Gripper Cont",resolution=0.01,command=update_env)
+w4.pack()
+
+while True:
+    env.render()
+    root.update()
