ff50264f9dcca6a7befde8c59f09da68362f3039
WarrG3X <warrier.abhishek@gmail.com>
2018-06-15 13:12:25 +0530

    Fixed Dobot Grip

--- pickAndPlace.py
+++ pickAndPlace.py
@@ -2,9 +2,9 @@ import gym
 from gym_dobot.envs import DobotPickAndPlaceEnv
 
 env = DobotPickAndPlaceEnv()
-for i_episode in range(10):
+while True:
     observation = env.reset()
-    while True:
+    for i in range(200):
         env.render()
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
