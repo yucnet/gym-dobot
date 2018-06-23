c2ab00355bbe7e4696d2b816aefd63cf41d37da3
WarrG3X <warrier.abhishek@gmail.com>
2018-06-23 07:54:52 +0530

    Added Tray and Blocks to Clutter Env

--- gym_dobot/envs/clutter.py
+++ gym_dobot/envs/clutter.py
@@ -1,8 +1,8 @@
 from gym import utils
-from gym_dobot.envs import dobot_env
+from gym_dobot.envs import clutter_env
 
 
-class DobotClutterPickEnv(dobot_env.DobotEnv, utils.EzPickle):
+class DobotClutterPickEnv(clutter_env.ClutterEnv, utils.EzPickle):
     def __init__(self, reward_type='sparse'):
         initial_qpos = {
             'dobot:slide0': 0.8,
@@ -10,7 +10,7 @@ class DobotClutterPickEnv(dobot_env.DobotEnv, utils.EzPickle):
             'dobot:slide2': -0.04,
             'object0:joint': [1.25, 0.53, 0.821,  1., 0., 0., 0.],
         }
-        dobot_env.DobotEnv.__init__(
+        clutter_env.ClutterEnv.__init__(
             self, 'dobot/clutter_pick.xml', has_object=True, block_gripper=False, n_substeps=20,
             gripper_extra_height=0, target_in_the_air=True, target_offset=.0,
             obj_range=0.185, target_range=0.2, distance_threshold=0.05,
