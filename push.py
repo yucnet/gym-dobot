b8d5e739c360e7f061d3746ee4fbcc7276b5c8b7
WarrG3X <warrier.abhishek@gmail.com>
2018-06-29 05:57:54 +0530

    Experimental

--- push.py
+++ push.py
@@ -1,10 +1,17 @@
 import gym
 from gym_dobot.envs import DobotPushEnv
+import mujoco_py
+from scipy import misc
 
 env = DobotPushEnv()
-while True:
+viewer = mujoco_py.MjViewer(env.sim)
+for j in range(1000):
+    img = viewer._read_pixels_as_in_window()
+    misc.imsave('test.png',img)
     observation = env.reset()
-    for i in range(1000000):
+    img = viewer._read_pixels_as_in_window()
+    misc.imsave('imgs/test{}.png'.format(j),img)
+    for i in range(50):
         env.render()
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
